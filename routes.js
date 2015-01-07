bd677283e6cfac4d6730830d971bc91bd45cd23e
thiagokroger <thiagokroger@gmail.com>
2015-01-07 10:06:08 -0200

    go

--- routes.js
+++ routes.js
@@ -6,11 +6,15 @@ module.exports = function(app) {
     router.post('/nasp', NaspController.update);
     router.get('/nasp/:email', NaspController.get);
 
-  var StatsController = require("./stats");
+    var StatsController = require("./stats");
 
 
-  router.get('/stats/active-users', StatsController.getActiveUsers);
-  router.get('/stats/active-users-length', StatsController.getActiveUsersLength);
+    router.get('/stats/active-users', StatsController.getActiveUsers);
+    router.get('/stats/active-users-length', StatsController.getActiveUsersLength);
+    router.get('/stats/apple-ingestion-monthly/:date', StatsController.getAppleIngestionMonthly);
+    router.get('/stats/apple-ingestion-monthly', StatsController.getAppleIngestionMonthly);
+    router.get('/stats/grade-by-month', StatsController.getGradeByMonth);
+
+
     return router;
 };
-
--- stats.js
+++ stats.js
@@ -2,6 +2,20 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 AWS.config.update({region: 'us-east-1'});
 var dynamodb = new AWS.DynamoDB();
+var fs = require("fs");
+
+function getFile(file){
+    return fs.readFileSync(file, { encoding: 'utf8' });
+}
+function getStorage(file){
+    return JSON.parse(getFile("./files/storage/"+file+".json"));
+}
+function setStorage(file, data){
+    fs.writeFile("./files/storage/"+file+".json", JSON.stringify(data), { encoding: 'utf8' });
+}
+function getTemplate(file){
+    return JSON.parse(getFile("./files/templates/"+file+".json"));
+}
 
 module.exports = {
     update: function (req, res, next) {
@@ -56,71 +70,132 @@ module.exports = {
             else     res.json(data);           // successful response
         });
     },
-  getActiveUsers : function(req, res, next){
-    var arr = [];
-    var lastWritten, interval;
-    var now = Math.round(+new Date()/1000);
-    dynamodb.scan({
-      TableName : 'Users'
-    }, function(err, data) {
-      if (err) { console.log(err); return; }
+    getActiveUsers : function(req, res, next){
+        var arr = [];
+        var lastWritten, interval;
+        var now = Math.round(+new Date()/1000);
+        dynamodb.scan({
+            TableName : 'Users'
+        }, function(err, data) {
+            if (err) { console.log(err); return; }
 
 
-      interval = 2628000;
-      console.log( (now - interval));
-      for (var ii in data.Items) {
+            interval = 2628000;
+            console.log( (now - interval));
+            for (var ii in data.Items) {
 
-        lastWritten = parseInt(data.Items[ii].LastWritten.N);
+                lastWritten = parseInt(data.Items[ii].LastWritten.N);
 
-        if (lastWritten > (now - interval)){
+                if (lastWritten > (now - interval)){
 
-          arr.push(data.Items[ii]);
-        }
-
-
-      }
-      res.json(arr);
-    });
-  },
-  getActiveUsersLength : function(req, res, next){
-    var arr = [];
-    var lastWritten, interval;
-    var now = Math.round(+new Date()/1000);
-    dynamodb.scan({
-      TableName : 'Users'
-    }, function(err, data) {
-      if (err) { console.log(err); return; }
-      var ctAll = 0;
-      var ct = 0;
-      interval = 2628000;
-      console.log( (now - interval));
-      for (var ii in data.Items) {
-
-        lastWritten = parseInt(data.Items[ii].LastWritten.N);
-        ctAll++;
-        if (lastWritten > (now - interval)){
-
-          arr.push(data.Items[ii]);
-          ct++;
-        }
+                    arr.push(data.Items[ii]);
+                }
 
 
-      }
-      res.json({all:ctAll, active:ct});
-    });
-  },
-    update3s: function (req, res, next) {
-
-
-        dynamodb.listTables({ "Limit": 12}, function(err, res) {
-            if(err)
-                console.log(err);
-            else {
-                console.log('ListTable from "test":');
-                console.log(res);
             }
+            res.json(arr);
+        });
+    },
+    getActiveUsersLength : function(req, res, next){
+        var arr = [];
+        var lastWritten, interval;
+        var now = Math.round(+new Date()/1000);
+        dynamodb.scan({
+            TableName : 'Users'
+        }, function(err, data) {
+            if (err) { console.log(err); return; }
+            var ctAll = 0;
+            var ct = 0;
+            interval = 2628000;
+            console.log( (now - interval));
+            for (var ii in data.Items) {
+
+                lastWritten = parseInt(data.Items[ii].LastWritten.N);
+                ctAll++;
+                if (lastWritten > (now - interval)){
+
+                    arr.push(data.Items[ii]);
+                    ct++;
+                }
+
+
+            }
+            res.json({all:ctAll, active:ct});
+        });
+    },
+    getAppleIngestionMonthly: function (req, res, next) {
+
+        var date = (req.params.date==undefined) ? "201501" : req.params.date;
+
+        var year = date.substr(0,4);
+        var month = date.substr(4,2);
+
+        var autoingestion = require("apple-autoingestion").AutoIngestion({
+            username: "applereport@starlinetecnologia.com.br",
+            password: "T%u8Hx90",
+            vendorId: "86119770"
         });
 
+        autoingestion.downloadSalesReport("Monthly", "Sales", "Summary", date, "./files", function (error, filePath) {
+            var storage = getStorage("apple-ingestion-monthly");
+            var template = getTemplate("highchart");
+
+            if (!error) {
+
+                date = year+"/"+month;
+
+                var arr = getFile(filePath).split("\n");
+
+                fs.unlink(filePath, function (err) {
+                    if (err) throw err;
+                });
+                fs.unlink(filePath+".gz", function (err) {
+                    if (err) throw err;
+                });
+
+                var unitIndex = 0;
+                var identifierIndex = 0;
+
+                var arrHeaders = arr[0].split("\t");
+
+                for (var x=0 ; x<arrHeaders.length ; x++){
+                    if (arrHeaders[x].toLowerCase()=="units"){
+                        unitIndex = x;
+                    }else if (arrHeaders[x].toLowerCase()=="product type identifier"){
+                        identifierIndex = x;
+                    }
+                }
+
+                var totalUnits = 0;
+
+                for (var x=1 ; x<arr.length ; x++){
+                    var arrValues = arr[x].split("\t");
+
+                    if (arrValues[identifierIndex]=="1F"){
+                        totalUnits += parseInt(arrValues[unitIndex]);
+                    }
+                }
+
+                storage[date] = totalUnits;
+
+                setStorage("apple-ingestion-monthly", storage);
+
+
+            } else {
+                console.log("Error: " + error);
+            }
+
+            for (var key in storage){
+                template.series[0].data.push(storage[key]);
+                template.xAxis.categories.push(key);
+            }
+            res.json(template);
+        });
+
+    },
+    getGradeByMonth: function (req, res, next) {
+        var storage = getStorage("grade-by-month");
+        res.json(storage);
     }
 
 }
